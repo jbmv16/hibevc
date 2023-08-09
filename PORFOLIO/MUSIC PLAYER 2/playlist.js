8d51b41301631a5e895a5d11c57ffcab735c5f6b
jbmv16 <115991030+jbmv16@users.noreply.github.com>
2023-08-09 20:31:33 +0800

    Add files via upload

---
+++ PORFOLIO/MUSIC PLAYER 2/playlist.js
@@ -0,0 +1,15 @@
+let songs = [
+	{
+		name : "From the inside out",
+		artist : "Hillsong",
+		img : "img1", 
+		music : "fromtheinsideout"
+	},
+	{
+		name : "Give Faith",
+		artist : "Hillsong2",
+		img : "img2", 
+		music : "givemefaith"
+	}
+
+]
